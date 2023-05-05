e45b0cdbbbe83cced74dd8059544563f5c5ef491
Daniel Roldan <dmroldanch@gmail.com>
2023-05-05 18:01:35 -0400

    3 Options

--- app/src/main/java/com/aptivist/training0423compose/ui/login/LoginVIewModel.kt
+++ app/src/main/java/com/aptivist/training0423compose/ui/login/LoginVIewModel.kt
@@ -1,5 +1,7 @@
 package com.aptivist.training0423compose.ui.login
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -12,39 +14,52 @@ import kotlinx.coroutines.launch
 class LoginVIewModel(
 ) : ViewModel()  {
 
-    /* Ya no lo haga compa
 
-    Opcion 1
-    private var _user: MutableState<String?> = mutableStateOf("")
-    val user : State<String?>
+    //Opcion 1
+    private var _user: MutableState<String> = mutableStateOf("")
+    val user : State<String>
         get() = _user
 
-    private var _password: MutableState<String?> = mutableStateOf("")
-    val password : State<String?>
+    private var _password: MutableState<String> = mutableStateOf("")
+    val password : State<String>
         get() = _password
 
 
+    //Opcion 2
 
-    Opcion 2
-        */
+    private var _loginValue1: MutableState<LoginValues> = mutableStateOf(LoginValues())
+    val loginValue1 : State<LoginValues>
+        get() = _loginValue1
 
-    /*private var _loginValue: MutableState<LoginValues> = mutableStateOf(LoginValues())
-    val loginValue : State<LoginValues>
-        get() = _loginValue*/
-
-
+    //Opcion 3
     var loginValue: LoginValues by mutableStateOf(LoginValues())
         private set
 
-    init {
 
+
+    init {
     }
 
     fun obtainUserDataFromView(user: String) {
-        loginValue = loginValue.copy(user=user)
+
+       //Option 1
+        _user.value = user
+
+        //Option 2
+        _loginValue1.value = _loginValue1.value.copy(user = user)
+
+        //Option 3
+        loginValue = loginValue.copy(user = user)
     }
 
     fun obtainPasswordDataFromView(password: String) {
+        //Option 1
+        _password.value = password
+
+        //Option 2
+        _loginValue1.value = _loginValue1.value.copy(password = password)
+
+        //Option 3
         loginValue = loginValue.copy(password = password)
     }
 
